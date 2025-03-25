3130066e12424ddb8a5d83c9222294ac54354bfd
Martin Šíl <mail@martinsil.cz>
2025-03-25 13:03:11 +0100

    Adding Supabase database types

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "./database.types";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY!;
 
-export const getSupabase = () => createClient(supabaseUrl, supabaseServiceKey);
+export const getSupabase = () =>
+	createClient<Database>(supabaseUrl, supabaseServiceKey);
