5fabac9639b1d2d9d608bf54e233a43d13b4d56d
Martin Šíl <mail@martinsil.cz>
2025-03-25 13:02:54 +0100

    Fetching the latest inquiry from Webtrh

--- app/api/webtrh/route.ts
+++ app/api/webtrh/route.ts
@@ -1,6 +1,8 @@
+import axios from "axios";
 import { headers } from "next/headers";
+import * as cheerio from "cheerio";
 
-export async function GET(req: Request) {
+export async function GET() {
 	const headersList = await headers();
 	const hasAccessKey = headersList.has("accesskey");
 
@@ -13,6 +15,22 @@ export async function GET(req: Request) {
 	if (accessKeyValue != process.env.ACCESS_KEY) {
 		return new Response("Heslo není správné", { status: 401 });
 	}
+
+	// Fetch Webtrh
+	const { data: html } = await axios.get("https://webtrh.cz/poptavky/");
+	const $ = cheerio.load(html);
+
+	// Find latest inquiry
+	const latestInquiryBox = $(".inquiry-box.unread").first();
+	const title = latestInquiryBox.find(".title span").first().text().trim();
+	console.log(title);
+   
+	// Compare it to the last saved in Supabase database
+	// If it is the same = end
+	// If its different, query the detail
+	// Send the detail to OpenAI to return true or false if its relevant to me
+	// If it is not = end
+	// If it is = send notification to Pushover app on my iPhone
 
 	return new Response("Vše v pořádku", { status: 200 });
 }
